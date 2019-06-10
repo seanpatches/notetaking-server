5c7082584cf62aacae61ac8117625b7ea6cfe258
Sean Nelson <smnelsonn@gmail.com>
2019-06-10 14:16:10 -0700

    working .post

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-console.log(require('dotenv').config());
 require('./lib/utils/connect')();
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 8889;
 
 app.listen(PORT, () => {
-  console.log(`connect at ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
---
+++ tests/routes/notes.test.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const request = require('supertest');
+const mongoose = require('mongoose');
+const connect = require('../../lib/utils/connect');
+const app = require('../../lib/app');
+
+describe('Note route tests', ()=> {
+  beforeAll(() => {
+    return connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('creates a note', () => {
+    return request(app)
+      .post('/notes')
+      .send({ title: 'titled', body: 'bodied' })
+      .then(res => {
+        expect(res.body).toEqual({
+          title: 'titled',
+          body: 'bodied',
+          __v: 0,
+          _id: expect.any(String)
+        });
+      });
+  });
+});
